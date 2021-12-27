5bb7a3e408fb231fc37af3639868c3023680f9f1
anshulShende <anshulshende40@gmail.com>
2021-12-27 11:09:44 +0530

    Added Api to fetch my Feed

--- backend/controllers/feedController.js
+++ backend/controllers/feedController.js
@@ -1,22 +1,26 @@
-const Tweet = require('../models/tweetModel')
+const Tweet = require('../models/tweetModel');
+const User = require('../models/userModel');
 
 const toggleLikeforSpecificTweet = async (req, res) => {
+  console.log(req.body.tweetId);
+  console.log(req.body.userId);
     try {
-      const tweet = await Tweet.findById(req.tweetId);
-
+      const tweet = await Tweet.findById(req.body.tweetId);
+      console.log(tweet);
       if (tweet==null || tweet.length == 0) {
         return res.status(400).json({ message: 'Tweet does not exist' });
       }
-      const liked = tweet.likes.indexOf(req.userId);
+      const liked = tweet.likes.indexOf(req.body.userId);
+      console.log(liked);
 
       if (liked === -1) {
-        tweet.likes.push(req.userId)
+        tweet.likes.push(req.body.userId);
       } else {
         tweet.likes.splice(liked, 1)
       }
 
       await tweet.save();
-      return res.status(200).json({message: "Likes for tweet updated successfully", likes: tweet.likes.count()});
+      return res.status(200).json({message: "Likes for tweet updated successfully", likes: tweet.likes.length});
     } catch (err) {
         return res.status(400).json({ message: 'Error Occurred.. Please try Again' });
     }
@@ -24,24 +28,122 @@ const toggleLikeforSpecificTweet = async (req, res) => {
 
 const toggleRetweetforSpecificTweet = async (req, res) => {
   try {
-    const tweet = await Tweet.findById(req.tweetId);
+    const tweet = await Tweet.findById(req.body.tweetId);
 
     if (tweet==null || tweet.length == 0) {
       return res.status(400).json({ message: 'Tweet does not exist' });
     }
-    const retweets = tweet.retweet.indexOf(req.userId);
+    const isReTweeted = tweet.retweet.indexOf(req.body.userId);
 
-    if (retweets === -1) {
-      tweet.retweets.push(req.userId)
+    if (isReTweeted === -1) {
+      tweet.retweet.push(req.body.userId);
     } else {
-      tweet.retweets.splice(liked, 1)
+      tweet.retweet.splice(isReTweeted, 1);
     }
 
     await tweet.save();
-    return res.status(200).json({message: "Retweets for tweet updated successfully", likes: tweet.retweets.count()});
+    return res.status(200).json({message: "Retweets for tweet updated successfully", retweets: tweet.retweet.length});
   } catch (err) {
       return res.status(400).json({ message: 'Error Occurred.. Please try Again' });
   }
 }
 
-module.exports = { toggleLikeforSpecificTweet, toggleRetweetforSpecificTweet };
+
+const followUser = async (req, res) => {
+  myUserId = req.body.myUserId;
+  followUserId = req.body.followUserId;
+
+  /** User -> followers **/
+  const user = await User.findById(followUserId);
+
+  if (!user || user == null || user.length == 0) {
+    return res.status(400).json({ message: 'User does not exist' });
+  }
+
+  if (user.followers.indexOf(myUserId) !== -1) {
+    return res.status(200).json({ message: `You already follow ${user.name}` });
+  }
+  user.followers.push(myUserId);
+  await user.save();
+
+  /** My -> Following **/
+  const me = await User.findById(myUserId);
+
+  me.follows.push(followUserId);
+  await me.save()
+
+  return res.status(200).json({message: `You are now following ${user.name}`});
+}
+
+const unfollowUser = async (req, res) => {
+  myUserId = req.body.myUserId;
+  unfollowUserId = req.body.unfollowUserId;
+
+  /** User -> followers **/
+  const user = await User.findById(unfollowUserId);
+
+  if (!user || user == null || user.length == 0) {
+    return res.status(400).json({ message: 'User does not exist' });
+  }
+
+  const isfollowed = user.followers.indexOf(myUserId);
+  if (isfollowed === -1) {
+    return res.status(400).json({ message: `You don't follow ${user.name}` });
+  }
+  user.followers.splice(isfollowed,1);
+  await user.save();
+
+  /** My -> Following **/
+  const me = await User.findById(myUserId);
+
+  const isfollows = me.follows.indexOf(unfollowUserId);
+  me.follows.splice(isfollows,1);
+  await me.save();
+
+  return res.status(200).json({message: `You unfollowed ${user.name}`});
+}
+
+const fetchMyfeed = async (req, res) => {
+  const userId = req.params.userId;
+
+  try{
+    const user = await User.findById(userId);
+    console.log(user);
+    if(!user || user == null || user.length == 0){
+      return res.status(400).json({ message: 'User does not exist' });
+    }
+
+    if(!user.isLoggedIn){
+      res.status(401).json({message: "Please Login to fetch your feed and updates"});
+    }
+
+    const followings = user.follows;
+    if(followings ==null || followings.length == 0){
+      res.status(200).json({ tweets: [], message: "Start following other users to get updates on your feed"});
+    }
+
+    const tweets = await Tweet.find({'userId':  { $in: followings } });
+
+    if(tweets==null || tweets.length == 0 ){
+      res.status(200).json({ tweets: [], message: "No Updates Found.. Keep following other users to get updates on your feed"});
+    }
+    
+    if(tweets.length > 1){
+       tweets.sort((a, b) => {
+          let ca = new Date(a.createdAt),
+          cb = new Date(b.createdAt);
+          return cb-ca;
+        });
+    }
+   
+    console.log(tweets);
+
+    res.status(200).json({ tweets: tweets, message: `Successfullly fetched Feed for ${user.name}`});
+
+  } catch(err) {
+    res.status(400).json({ message: `Error Occurred while fetching Feed for ${userId}`});
+  }
+
+}
+
+module.exports = { toggleLikeforSpecificTweet, toggleRetweetforSpecificTweet, followUser, unfollowUser, fetchMyfeed };
--- backend/routes/feedRoutes.js
+++ backend/routes/feedRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { toggleLikeforSpecificTweet, toggleRetweetforSpecificTweet } = require("../controllers/feedController");
+const { toggleLikeforSpecificTweet, toggleRetweetforSpecificTweet, followUser, unfollowUser, fetchMyfeed } = require("../controllers/feedController");
 
 // @Mapping("/feed")
 router.post("/like", toggleLikeforSpecificTweet);
 router.post("/retweet", toggleRetweetforSpecificTweet);
+router.post("/follow", followUser);
+router.post("/unfollow", unfollowUser);
+router.get("/:userId", fetchMyfeed);
 
 
 module.exports = router;
